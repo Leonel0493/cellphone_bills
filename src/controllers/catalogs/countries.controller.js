f6761da6a055d14bfccba078f634dea79cfb446f
Leonel Rivas <leonrivas04@gmail.com>
2023-02-23 13:08:08 -0600

    delete
    fix: delete unnecessary files

--- src/controllers/catalogs/countries.controller.js
+++
@@ -1,8 +0,0 @@
-import { Countries } from '../../models/catalogs/countries.model.js'
-
-export const GetAll = async (req, res) => {
-  console.log('Hola')
-  const countries = await Countries.findAll();
-  console.log("countries");
-  res.json(countries);
-} 
--- src/routes/catalogs/countries.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { tokenValidator } from "../../middlewares/authoritation.js";
-import { GetAll } from "../../controllers/catalogs/countries.controller.js";
-
-const router = Router();
-
-router.get("/all", await tokenValidator, GetAll);
-
-export default router;
