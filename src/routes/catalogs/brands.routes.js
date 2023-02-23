53f7703321147b1bfeb1d0ad479eb091edd996a5
Leonel Rivas <leonrivas04@gmail.com>
2023-02-23 13:30:49 -0600

    feat
    Add routes files, this endpoints it's only for CRUD actions

---
+++ src/routes/catalogs/brands.routes.js
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import { tokenRoleValidator } from '../../middlewares/authoritation';
+
+const router = Router();
+
+router.get('/all', await tokenRoleValidator('Admin'), () => console.log('get all brands'));
+router.get('/{id}', await tokenRoleValidator('Admin'),  () => console.log('get brand by id'));
+router.post('/', await tokenRoleValidator('Admin'), () => console.log('post'));
+router.put('/', await tokenRoleValidator('Admin'), () => console.log('put'));
+router.delete('/', await tokenRoleValidator('Admin'), () => console.log('delete'));
+
+export default router;
---
+++ src/routes/catalogs/models.routes.js
@@ -0,0 +1,12 @@
+import {Router} from 'express';
+import { tokenRoleValidator } from '../../middlewares/authoritation';
+
+const router = Router();
+
+router.get('/all', await tokenRoleValidator('Admin'), () => console.log('get all models'));
+router.get('/{id}', await tokenRoleValidator('Admin'),  () => console.log('get model by id'));
+router.post('/', await tokenRoleValidator('Admin'), () => console.log('post'));
+router.put('/', await tokenRoleValidator('Admin'), () => console.log('put'));
+router.delete('/', await tokenRoleValidator('Admin'), () => console.log('delete'));
+
+export default router;
